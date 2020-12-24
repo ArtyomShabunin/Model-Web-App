143020257757e8cc74fc743bd25d0849d5f5aafb
Atryom Shabunin <asshabunin@vti.ru>
2020-12-24 21:37:46 +0300

    Работа на асинхронностью

--- restmodel/app/simulation.py
+++ restmodel/app/simulation.py
@@ -16,6 +16,11 @@
 
 import os
 from subprocess import Popen, PIPE
+import asyncio
+from pymodbus.client.asynchronous.tcp import AsyncModbusTCPClient as ModbusClient
+from pymodbus.client.asynchronous import schedulers
+import numpy as np
+
 # from datetime import timedelta
 
 class Simulation():
@@ -25,6 +30,9 @@ class Simulation():
         self.SIT_start_pattern = r'c:\SimInTech64\bin\mmain.exe  "..\simintech\modbus_control\modbus_control.prt" /setparameter COMMAND_file_name {} /nomainform /hide /run'
 
         #Подключение к Modbus
+        self.modbus_host = "127.0.0.1"
+        self.modbus_control_port = 1502
+        self.modbus_model_port = 1503
         # self.master_control = modbus_tcp.TcpMaster(host='localhost',port=1502)
 
     def model_initialisation(self):
@@ -38,14 +46,65 @@ class Simulation():
 
         self.model_initialised = False
 
+    def restart_model(self):
+        pass
 
-    # def modelselection(self):
-    #     name = self.cursor.execute("""SELECT name FROM Modelselection""")
-    #     description  = self.cursor.execute("""SELECT description  FROM Modelselection""")
-    #     name_model = []
-    #     for row in name:
-    #         name_model += row
-    #     for row in description:
-    #         name_model += row
-    #     name_model = str(name_model)
-    #     return name_model
+    def start_model(self):
+        pass
+
+    def pause_model(self):
+        pass
+
+    def start_model(self):
+        pass
+
+    def update_data(self):
+        pass
+
+    def connect_to_control_modbus(self):
+        pass
+
+    def connect_to_model_modbus(self):
+        pass
+
+    async def start_async_test(self, client):
+
+        reg_count = 4714
+        reg_count = 125
+        bit_count = 5475
+        max_reg = 125
+        max_bit = 2000
+
+        data = []
+
+        for i in range(10):
+
+            for start in range(0,reg_count,max_reg):
+                number = min(reg_count-start, max_reg)
+                rr = await client.read_holding_registers(start, number, unit=1)
+
+                result = rr.registers
+                data += result
+
+            print('Done!')
+
+            await asyncio.sleep(1)
+
+            # print(rr)
+            # print(rr.registers)
+            print(np.array(data, dtype=np.int16).view(dtype = np.float32))
+            print()
+
+    def run_with_no_loop(self):
+        """
+        ModbusClient Factory creates a loop.
+        :return:
+        """
+        loop, client = ModbusClient(schedulers.ASYNC_IO, port=self.modbus_model_port)
+        loop.run_until_complete(self.start_async_test(client.protocol))
+        loop.close()
+
+if __name__ == '__main__':
+    file_name = r"..\..\simintech\model\pac\boiler_model_auxsteam.pak"
+    sim = Simulation(file_name)
+    sim.model_initialisation()
