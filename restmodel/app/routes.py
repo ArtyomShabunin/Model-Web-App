a05631ed7e31b7712a9db5907ee4d750202fa7a4
unknown <shabunin_a@mail.ru>
2021-01-14 08:57:22 +0300

    Изменения в чтении сигналов по Modbus

--- restmodel/app/routes.py
+++ restmodel/app/routes.py
@@ -1,86 +1,86 @@
 from flask import Flask
 from app import app, db
 from app.models import *
-from app.model import Model
+# from app.model import Model
 
 model = Model()
 
 
-@app.route('/modelselection',methods = ['GET'])
-def Modelselection():
-    name_model=model.modelselection()
-    return name_model
-
-
-#Запрос открытия модели тренажера
-@app.route('/run_model/<string:name>',methods = ['POST'])
-def INITMODEL(name):
-    print('Кнопка ОТКРЫТЬ нажата')
-    model.init(name)
-    return 'Run'
-
-
-#Запрос запуска проекта
-@app.route('/start_model',methods = ['PUT'])
-def START():
-    print('Кнопка СТАРТ нажата')
-    model.start()
-    return 'Start'
-
-
-#Запрос паузы проекта
-@app.route('/pause_model',methods = ['PUT'])
-def PAUSE():
-    print('Кнопка Пауза нажата')
-    model.pause_model()
-    return 'Pause'
-
-#Запрос остановки проекта
-@app.route('/stop_model',methods = ['PUT'])
-def STOP():
-    print('Кнопка СТОП нажата')
-    model.stop()
-    return 'Stop'
-
-
-
-#Запрос сохранения рестарта
-@app.route('/save_restart_model',methods = ['POST'])
-def save_restart():
-    print('Кнопка Сохранить рестарт нажата')
-    model.save_restart()
-    return 'Save Restart'
-
-#Запрос запуск с рестарта
-@app.route('/read_restart_model',methods = ['PUT'])
-def read_restart():
-    print('Кнопка Открыть рестарт нажата')
-    model.read_restart()
-    return 'Read Restart'
-
-
-#Запись переменных в базу данных
-@app.route('/variable_to_bd/<string:name>',methods = ['POST'])
-def variable_to_bd_model(name):
-    print('Переменные записаны в базу данных')
-    model.create_model(name)
-    return 'Variable to bd'
-
-
-#Передача имен переменных в DASH
-@app.route('/variable', methods = ['GET'])
-def variable():
-    funct = model.variable()
-    print('Переменные переданы в выпадающий список')
-    return funct
-
-
-
-#Запрос построения графиков
-@app.route('/measurement_signals/<id>', methods = ['GET'])
-def build_graph(id):
-    funct = model.measurement_signals(id)
-    return funct
+# @app.route('/modelselection',methods = ['GET'])
+# def Modelselection():
+#     name_model=model.modelselection()
+#     return name_model
+#
+#
+# #Запрос открытия модели тренажера
+# @app.route('/run_model/<string:name>',methods = ['POST'])
+# def INITMODEL(name):
+#     print('Кнопка ОТКРЫТЬ нажата')
+#     model.init(name)
+#     return 'Run'
+#
+#
+# #Запрос запуска проекта
+# @app.route('/start_model',methods = ['PUT'])
+# def START():
+#     print('Кнопка СТАРТ нажата')
+#     model.start()
+#     return 'Start'
+#
+#
+# #Запрос паузы проекта
+# @app.route('/pause_model',methods = ['PUT'])
+# def PAUSE():
+#     print('Кнопка Пауза нажата')
+#     model.pause_model()
+#     return 'Pause'
+#
+# #Запрос остановки проекта
+# @app.route('/stop_model',methods = ['PUT'])
+# def STOP():
+#     print('Кнопка СТОП нажата')
+#     model.stop()
+#     return 'Stop'
+#
+#
+#
+# #Запрос сохранения рестарта
+# @app.route('/save_restart_model',methods = ['POST'])
+# def save_restart():
+#     print('Кнопка Сохранить рестарт нажата')
+#     model.save_restart()
+#     return 'Save Restart'
+#
+# #Запрос запуск с рестарта
+# @app.route('/read_restart_model',methods = ['PUT'])
+# def read_restart():
+#     print('Кнопка Открыть рестарт нажата')
+#     model.read_restart()
+#     return 'Read Restart'
+#
+#
+# #Запись переменных в базу данных
+# @app.route('/variable_to_bd/<string:name>',methods = ['POST'])
+# def variable_to_bd_model(name):
+#     print('Переменные записаны в базу данных')
+#     model.create_model(name)
+#     return 'Variable to bd'
+#
+#
+# #Передача имен переменных в DASH
+# @app.route('/variable', methods = ['GET'])
+# def variable():
+#     funct = model.variable()
+#     print('Переменные переданы в выпадающий список')
+#     return funct
+#
+#
+#
+# #Запрос построения графиков
+# @app.route('/measurement_signals/<id>', methods = ['GET'])
+# def build_graph(id):
+#     funct = model.measurement_signals(id)
+#     return funct
 
 
 
--- restmodel/app/simulation.py
+++ restmodel/app/simulation.py
@@ -55,20 +55,26 @@ class Simulation():
         self.modbus_control_port = 1502
         self.modbus_model_port = 1503
 
-        # Подключение к базе данных
-        self.conn = create_engine('postgresql://postgres:postgres@127.0.0.1:6543/variable')
-        self.cursor = self.conn.connect()
-
     def model_initialisation(self, model_name:str):
 
         self.model_filename = Model.query.filter_by(name=model_name).first().filename
         model_id = Model.query.filter_by(name=model_name).first().id
 
-        self.measurements_count_1 = Measurementvariable.query.filter_by(model_id=model_id).filter_by(modbus_unit=1).count()
-        self.signals_count = Measurementvariable.query.filter_by(model_id=model_id).count()
-		# Запуск SimInTech
+        # Число измерений
+        self.measurements_count = Measurementvariable.query.filter_by(model_id=model_id).count()
+        # Число сигналов для чтения (статус задвижек, механизмов, регуляторов)
+        self.signals_count = Signalvariable.query.filter_by(model_id=model_id).count()
+        # # Число измерений для записи (задания регуляторов, ЧРП)
+        # self.measurements_count_2 = Measurementvariable.query.filter_by(model_id=model_id).filter_by(modbus_unit=4).count()
+        # # Число сигналов для чтения (статус задвижек, механизмов, регуляторов)
+        # self.signals_count_2 = Signalvariable.query.filter_by(model_id=model_id).filter_by(modbus_unit=3).count()
+
+        self.reading_measurements = []
+        self.reading_signals = []
+
+        # Запуск SimInTech
         self.model_process = Popen(self.SIT_start_pattern.format(self.model_filename), stdout=PIPE, shell=True)
-        # Выдержка времени
+        # Выдержка времени!!!!!!!!!!!!!!!
         self.model_initialised = True
 
     def kill_model(self):
@@ -97,16 +103,51 @@ class Simulation():
     def connect_to_model_modbus(self):
         pass
 
+    async def reading_data(self, client):
+        max_reg = 125 # максимально число считываемых за раз регистров
+        max_bit = 2000
+
+        reading_measurements_reg = []
+        reading_signals_reg = []
+
+        meas_reg_count = self.measurements_count * 2
+        signals_reg_count = self.signals_count
+        signals_reg_count = 3000
+
+        # meas_reg_count = 100
+        # signals_reg_count = 200
+
+        for start in range(0,meas_reg_count,max_reg):
+            number = min(meas_reg_count-start, max_reg)
+            rr = await client.read_holding_registers(start, number, unit=1)
+            reading_measurements_reg += rr.registers
+
+        for start in range(0,signals_reg_count,max_bit):
+            number = min(signals_reg_count-start, max_bit)
+            rr = await client.read_coils(start, number, unit=1)
+            print("start {}".format(start))
+            reading_signals_reg += rr.bits
+            print("норм")
+
+        self.reading_measurements = np.array(reading_measurements_reg, dtype=np.int16).view(dtype = np.float32)
+
+        print(reading_measurements_reg[0:10])
+        print(reading_signals_reg[0:10])
+        self.reading_signals = reading_signals_reg
+
+
+
     async def start_async_test(self, client):
 
-        reg_count = self.measurements_count_1 * 2
+        reg_count = self.measurements_count * 2
         bit_count = 5475
         max_reg = 125
         max_bit = 2000
 
-        data = []
 
         for i in range(10):
+
+            data = []
 
             for start in range(0,reg_count,max_reg):
                 number = min(reg_count-start, max_reg)
@@ -124,13 +165,15 @@ class Simulation():
             print(np.array(data, dtype=np.int16).view(dtype = np.float32))
             print()
 
+
     def run_with_no_loop(self):
         """
         ModbusClient Factory creates a loop.
         :return:
         """
         loop, client = ModbusClient(schedulers.ASYNC_IO, port=self.modbus_model_port)
-        loop.run_until_complete(self.start_async_test(client.protocol))
+        # loop.run_until_complete(self.start_async_test(client.protocol))
+        loop.run_until_complete(self.reading_data(client.protocol))
         loop.close()
 
 if __name__ == '__main__':
