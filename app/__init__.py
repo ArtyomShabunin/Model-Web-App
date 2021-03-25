d77139b5535b3a8a01d6b95f329fc5ac00890988
Artyom Shabunin <shabunin_a@mail.ru>
2021-03-25 12:20:32 +0300

    Подключена библиотека QUART-CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ import quart.flask_patch
 
 # from flask import Flask
 from quart import Quart
+from quart_cors import cors
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 # from flask_migrate import Migrate
 
 app = Quart(__name__)
+app = cors(app, allow_origin="*")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
